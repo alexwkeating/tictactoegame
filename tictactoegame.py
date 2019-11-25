2dbc82b48c8e69596ce4e8727978fb543e2469c8
alexwkeating <awk634@mun.ca>
2019-11-25 13:33:05 -0330

    Check git

--- tictactoegame.py
+++ tictactoegame.py
@@ -1,5 +1,4 @@
-#check if git works
-#idk man
+#check git commit
 def display_board(board):
     print('\n'*100)
     print(f" {board[1]} | {board[2]} | {board[3]} \n-----------\n {board[4]} | {board[5]} | {board[6]} \n-----------\n {board[7]} | {board[8]} | {board[9]} ")
